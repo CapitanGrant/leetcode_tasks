5ede626b848f8cbd8bc7e244d26ec0e5eb8c3c42
CapitanGrant <121495258+CapitanGrant@users.noreply.github.com>
2025-02-08 14:49:23 +0300

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         num = 0
@@ -16,9 +19,27 @@ class Solution:
 
         return str(''.join(lst))
 
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        min_len = len(min(strs, key=len))
+        lcp = ""
+
+        for i in range(min_len):
+            char = strs[0][i]
+            for j in strs:
+                if i >= len(j) or j[i] != char:
+                    return lcp
+            lcp += char
+
+        return lcp
 
 sol = Solution()
 
-print(sol.addBinary('000100', '110010')) # 110110
-print(sol.addBinary('1010', '1011'))  # 10101
+print(sol.longestCommonPrefix(strs=["flower", "flow", "flight"]))
+print(sol.longestCommonPrefix(strs=["dog","racecar","car"]))
 
+
+# print(sol.addBinary('000100', '110010'))  # 110110
+# print(sol.addBinary('1010', '1011'))  # 10101

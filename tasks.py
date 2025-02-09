e59baf20efa64dc11334cbac349aebd2f5452994
CapitanGrant <121495258+CapitanGrant@users.noreply.github.com>
2025-02-09 17:45:07 +0300

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -19,6 +19,20 @@ class Solution:
 
         return str(''.join(lst))
 
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            guess = nums[mid]
+            if guess == target:
+                return mid
+            if guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
@@ -35,11 +49,43 @@ class Solution:
 
         return lcp
 
+    def isValid(self, s: str) -> bool:
+        pass
+
+    def mySqrt(self, x: int) -> int:
+        low = 0
+        high = x
+        while low <= high:
+            mid = (low + high) // 2
+            if mid ** 2 <= x <= (mid + 1) ** 2:
+                return mid
+            if mid ** 2 > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+    def missingNumber(self, nums: List[int]) -> int:
+        sum_num = sum(nums)
+        len_num = len(nums)
+        res_num = sum([i for i in range(0, len_num+1)])
+        return res_num - sum_num
+
+
+
 sol = Solution()
 
-print(sol.longestCommonPrefix(strs=["flower", "flow", "flight"]))
-print(sol.longestCommonPrefix(strs=["dog","racecar","car"]))
+print(sol.missingNumber([9,6,4,2,3,5,7,0,1])) # [0, 1, 3]
+# print(sol.mySqrt(9))
 
-
+# print(sol.isValid(s="()")) # true
+# print(sol.isValid(s="()[]{}")) # true
+# print(sol.isValid(s="(]")) # false
+#
+#
+# print(sol.longestCommonPrefix(strs=["flower", "flow", "flight"]))
+# print(sol.longestCommonPrefix(strs=["dog","racecar","car"]))
+#
+#
 # print(sol.addBinary('000100', '110010'))  # 110110
 # print(sol.addBinary('1010', '1011'))  # 10101

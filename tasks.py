00a78a7cb6c73d508760fdaf728e49b080a186c5
CapitanGrant <121495258+CapitanGrant@users.noreply.github.com>
2025-04-10 22:45:56 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -491,6 +491,26 @@ class Solution:
         result = [i[0] for i in res]
         return result
 
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left_sum = [0] * n
+        right_sum = [0] * n
+        answer = [0] * n
+
+
+        for i in range(1, n):
+            left_sum[i] = left_sum[i - 1] + nums[i - 1]
+
+
+        for i in range(n - 2, -1, -1):
+            right_sum[i] = right_sum[i + 1] + nums[i + 1]
+
+
+        for i in range(n):
+            answer[i] = abs(left_sum[i] - right_sum[i])
+
+        return answer
+
 sol = Solution()
 
 print(sol.removeDuplicates(nums = [0,0,1,1,1,2,2,3,3,4]))

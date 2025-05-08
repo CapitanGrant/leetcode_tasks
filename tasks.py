a5f12e9cdb36c6e6c57116f55d8da53c4998f6ac
CapitanGrant <121495258+CapitanGrant@users.noreply.github.com>
2025-05-08 09:45:28 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -555,6 +555,10 @@ class Solution:
     def theMaximumAchievableX(self, num: int, t: int) -> int:
         return num + 2 * t
 
+    def convertTemperature(self, celsius: float) -> List[float]:
+        Kelvin = celsius + 273.15
+        Fahrenheit = celsius * 1.80 + 32.00
+        return [Kelvin, Fahrenheit]
 
 
 sol = Solution()

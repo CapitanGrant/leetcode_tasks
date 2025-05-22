37bd36320432f9276fcd595cafe4e8c5146869aa
CapitanGrant <121495258+CapitanGrant@users.noreply.github.com>
2025-05-22 11:17:45 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -645,6 +645,29 @@ class Solution:
                 res.append(a[i])
         return res
 
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = []
+        i = 0
+        n = max(len(word1), len(word2))
+        while i < n:
+            if i < len(word1):
+                res.append(word1[i])
+
+            if i < len(word2):
+                res.append(word2[i])
+            i += 1
+        return ''.join(res)
+    
+    def mergeAlternately2(self, word1: str, word2: str) -> str:
+        merged = []
+
+        for a, b in zip(word1, word2):
+            merged.append(a + b)
+
+        merged.append(word1[len(word2):])
+        merged.append(word2[len(word1):])
+
+        return "".join(merged)
 
 sol = Solution()
 
